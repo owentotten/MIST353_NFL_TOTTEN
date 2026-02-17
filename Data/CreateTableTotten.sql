7e750799348b9795cd2c986f15dfe5d327299c59
owentotten <146154955+owentotten@users.noreply.github.com>
2026-02-17 12:23:46 -0500

    started inserting data

--- Data/CreateTableTotten.sql
+++ Data/CreateTableTotten.sql
@@ -1,1 +1,18 @@
---create tables for first iteration;
+-- create a database for NFL app
+USE master;
+GO
+ 
+-- Uncommented this so the database actually gets built
+CREATE DATABASE MIST353_NFL_RDB_Totten;
+GO
+ 
+USE MIST353_NFL_RDB_Totten;
+GO
+ 
+-- create tables for first iteration
+CREATE TABLE ConferenceDivision (ConferenceDivisionID INT IDENTITY(1,1) PRIMARY KEY, Conference NVARCHAR(50) , Division NVARCHAR(50));
+
+
+
+
+create table Team (TeamID INT IDENTITY(1,1) PRIMARY KEY, TeamName NVARCHAR(50), ConferenceDivisionID INT FOREIGN KEY REFERENCES ConferenceDivision(ConferenceDivisionID));
